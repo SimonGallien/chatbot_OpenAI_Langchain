4f26d2e327c026304eb9a277058d31b486de0477
SimonGallien <simongallien@orange.fr>
2025-08-18 09:58:42 +0200

    création de la chaine conversationnelle

--- api/main.py
+++ api/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
 from chain import summarize
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START, END
+from chain import (
+    State,
+    call_model,
+    summarize_history,
+    should_continue,
+    print_update,
+)
 
+chain = None
 app = FastAPI()
 
 
@@ -19,3 +31,48 @@ async def summarize_text(file: UploadFile = File(...)):
     summary = summarize(text)
 
     return {"summary": summary}
+
+
+@app.post("/initialize")
+async def initialize(file: UploadFile = File(...)):
+    global chain
+    if file.content_type != "text/plain":
+        return {"error": "Only text files are supported"}
+    content = await file.read()
+    text = content.decode("utf-8")
+
+    workflow = StateGraph(State)
+    workflow.add_node(
+        "conversation",
+        lambda input: call_model(state=input, conversation_summary=summary),
+    )
+    workflow.add_node(summarize_history)
+    workflow.add_edge(START, "conversation")
+    workflow.add_conditional_edges(
+        "conversation",
+        should_continue,
+    )
+    workflow.add_edge("summarize_history", END)
+
+    memory = MemorySaver()
+    chain = workflow.compile(checkpointer=memory)
+    summary = summarize(text)
+    return {"summarize": summary}
+
+
+async def generate_stream(input_message, config, chain):
+    for event in chain.stream(
+        {"messages": [input_message]}, config, stream_mode="updates"
+    ):
+        yield event["conversation"]["messages"][
+            0
+        ].content  # Envoie le contenu de l'évènement au fur et à mesure
+
+
+@app.post("/update")
+def update(request: str):
+    config = {"configurable": {"thread_id": "4"}}
+    input_message = HumanMessage(content=request)
+    return StreamingResponse(
+        generate_stream(input_message, config, chain), media_type="text/plain"
+    )
